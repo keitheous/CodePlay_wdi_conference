09ee363ab7218692b237d40349b49fc2e345352c
Emily Bie <bieyuyu@gmail.com>
2016-05-25 15:34:52 +1000

    seats api

--- app/controllers/api/seats_controller.rb
+++ app/controllers/api/seats_controller.rb
@@ -15,6 +15,30 @@ module Api
       render json: seat.to_json
     end
 
+    def reserve_seats
+      seat_numbers = params[:seats_num]
+      reserved_seats = []
+      seat_numbers.each do |seat_num|
+        seat = Seat.find_by(seat_num: seat_num)
+        seat.status = 'reserved'
+        seat.save
+        reserved_seats << seat
+      end
+      render json: reserved_seats.to_json
+    end
+
+    def revert_seats
+      seat_numbers = params[:seats_num]
+      revert_seats = []
+      seat_numbers.each do |seat_num|
+        seat = Seat.find_by(seat_num: seat_num)
+        seat.status = 'available'
+        seat.save
+        revert_seats << seat
+      end
+      render json: revert_seats.to_json
+    end
+
   end
 
 end

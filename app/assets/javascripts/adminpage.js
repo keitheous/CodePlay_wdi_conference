0beebd55ba821fbe9c89ba9d07d12621e04aab9e
Emily Bie <bieyuyu@gmail.com>
2016-05-26 01:41:48 +1000

    admin page fix bugs and email with background image

--- app/assets/javascripts/adminpage.js
+++ app/assets/javascripts/adminpage.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+
+  var undisplayApplicationTable = function() {
+     var applyingItems = $('.applying-item');
+     var $applyingTable = $('.applying-list');
+     // if all the item css display == none undisplay the table
+     var displayTable = false;
+     for(var i=0;i<applyingItems.length;i++) {
+        if (applyingItems.eq(i).css('display') != 'none') {
+          displayTable = true;
+        }
+     }
+     if (displayTable == false) {
+        $applyingTable.css({"display":"none"});
+        $(".applying-users-handler").append($("<p class='no-pending-msg'>There's no pending application.</p>"));
+     }
+  }
+
   $('.approve-button').on('click',function(event){
     var $applyingList = $(this).closest('.applying-item');
     var event_speaker_id = $applyingList.data('event-speaker-id');
@@ -15,13 +32,25 @@ $(document).ready(function() {
        var $tr = $('<tr>');
        var $speaker_name = $('<td>').html(data.speaker.name);
        var $topic = $('<td>').html(data.event_speaker.topic);
-       var $talk_time = $('<td>').html(data.hour+ ':'+data.min);
+       var $talk_time = $('<td>').html(data.hour+ ':'+ data.min);
        var $event_name = $('<td>').html(data.event.name);
+
+       // delete button
+       var templateString = $("#form-template").html();
+       var templateFunction = Handlebars.compile(templateString);
+       var html = templateFunction({
+        event_speaker_id: data.event_speaker.id,
+       });
+      var $deleteButton = $(html);
        $tr.append($speaker_name);
        $tr.append($topic);
        $tr.append($talk_time);
        $tr.append($event_name);
+       $tr.append($deleteButton);
        $('.speakers-list-form').append($tr);
+
+       // undisplay application table when there's no applying item
+      undisplayApplicationTable();
     });
   });
 
@@ -36,9 +65,7 @@ $(document).ready(function() {
       method: 'delete'
     }).done(function(response) {
       $applyingList.css('display','none');
-      if (response === null) {
-        $applyingTable.css('display','none');
-      }
+      undisplayApplicationTable();
     });
   });
 
@@ -47,8 +74,6 @@ $(document).ready(function() {
     $('.add-events-form').slideDown("fast");
     $('.events-application-container').fadeOut(100);
     $('#navmenu').hide();
-    // jQuery('#add-event-date-picker').datetimepicker();
-
   });
 
   $("#close-add-event").on('click',function(event) {
@@ -69,12 +94,14 @@ $(document).ready(function() {
       $('.edit-events-form').find("#event-time").val(response.time);
       $('.edit-events-form').slideDown("fast");
       $('.events-application-container').fadeOut(100);
+      $('#navmenu').hide();
     });
   });
 
   $("#close-edit-event").on('click',function(event){
     $('.edit-events-form').slideUp("fast");
     $('.events-application-container').fadeIn(500);
+    $('#navmenu').show(100);
   });
 
   // add hover text for edit/delete button
@@ -90,5 +117,12 @@ $(document).ready(function() {
   $('.event-delete-tbn').on('mouseout',function(event) {
     $(this).find('.delete-hover').css({"display":"none"});
   });
+
+  function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+  }
 
 });

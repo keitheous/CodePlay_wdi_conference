58b37e03cd513282af9b4a44b6aead78c85ced35
Emily Bie <bieyuyu@gmail.com>
2016-05-27 09:35:13 +1000

    fix bug of buy ticket seats

--- app/controllers/api/seats_controller.rb
+++ app/controllers/api/seats_controller.rb
@@ -2,7 +2,8 @@ module Api
   class SeatsController < ApplicationController
 
     def index
-      @seats = Seat.order(:id)
+
+      @seats = Seat.order(:id).where(event_id: 1)
       render json: @seats.to_json(include: [:ticket, :seat_type])
     end
 

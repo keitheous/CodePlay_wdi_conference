787528b4b754eff5589dba884a383117e8dff8ad
Christopher Lee <hanernlee@gmail.com>
2016-05-26 15:52:31 +1000

    styled order summary

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def login
-      @user = User.find_by(name: params[:name])
+      @user = User.find_by(email: params[:email])
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
         if @user.user_type.group == "admin"
